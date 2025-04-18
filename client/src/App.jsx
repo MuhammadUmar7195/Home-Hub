d5b556a8f01976483a97fbb6f81c2b47affedd4e
MuhammadUmar7195 <mmuhammadumar869@gmail.com>
2025-04-18 15:41:24 +0500

    configure a signup page ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,8 +13,8 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/profile" element={<Profile />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
