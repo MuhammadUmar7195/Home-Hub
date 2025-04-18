879058044d2b7e98f222d49a3b5b94aa620f1e90
MuhammadUmar7195 <mmuhammadumar869@gmail.com>
2025-04-18 17:31:30 +0500

    Adding a cors for access frontend port

--- server/app.js
+++ server/app.js
@@ -3,11 +3,19 @@ const app = express();
 const connectDB = require("./Config/connect");
 const status = require("express-status-monitor");
 require("dotenv").config();
+const cors = require("cors");
 
 //third-party middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(status());
+
+//add cors for accessing ports
+app.use(cors({
+    origin: "http://localhost:3000",
+    methods: ["POST", "GET"],
+    credentials: true 
+}));
 
 //environment variables
 const port = process.env.PORT;
