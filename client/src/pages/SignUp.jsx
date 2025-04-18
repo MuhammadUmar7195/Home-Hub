603a53d862a775aa5f129b7374d4489d22553756
MuhammadUmar7195 <mmuhammadumar869@gmail.com>
2025-04-18 17:27:14 +0500

    Add signup functionality or proxy setup

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,35 +1,82 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const SignUp = () => {
+  const [formData, setFormData] = useState({}); //initialize with empty object
+  const [error, setError] = useState(null); //initialize with null
+  const [loading, setLoading] = useState(false); //initialize with bool false
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const { data } = await axios.post("/api/auth/signup", formData, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      // console.log("Signup response:", data);
+      if (data?.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate("/signin");
+    } catch (error) {
+      setLoading(false);
+      console.log("Caught error:", error);
+      setError(
+        error.response?.data?.message || error.message || "Signup failed"
+      );
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-4xl text-center font-semibold my-7">Sign up</h1>
-      <form action="" className="flex flex-col gap-4 border-none">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4 border-none">
         <input
           type="text"
           placeholder="Username"
           className="border p-3 rounded-lg"
           id="username"
+          onChange={handleChange}
         />
         <input
           type="email"
           placeholder="Email"
           className="border p-3 rounded-lg"
           id="email"
+          onChange={handleChange}
         />
         <input
-          type="text"
+          type="password"
           placeholder="Password"
           className="border p-3 rounded-lg"
           id="password"
+          onChange={handleChange}
         />
-        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-80">
-          Sign up
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-80"
+        >
+          {loading ? `Loading...` : `Sign up`}
         </button>
       </form>
 
-      <div className="flex gap-2 mt-5">
+      {error && <p className="text-red-500 mt-4">{error}</p>}
+      <div className="flex gap-2 pt-5">
         <p>Have an account? </p>
         <Link to={`/signin`}>
           <span className="text-blue-700">Sign in</span>
