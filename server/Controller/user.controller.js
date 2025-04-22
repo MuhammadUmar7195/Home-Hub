776c22533b9d37627ade6bbac4fdbd96ce746013
MuhammadUmar7195 <mmuhammadumar869@gmail.com>
2025-04-22 15:27:28 +0500

    Create a user delete account api route

--- server/Controller/user.controller.js
+++ server/Controller/user.controller.js
@@ -18,7 +18,7 @@ const updateUser = async (req, res, next) => {
         };
 
         if (req.body.password) {
-            updateData.password = await argon2.hash(req.body.password);
+            updateData.password = await argon.hash(req.body.password);
         }
 
         const updatedUser = await User.findByIdAndUpdate(
@@ -39,7 +39,25 @@ const updateUser = async (req, res, next) => {
     }
 };
 
+
+const deleteUser = async (req, res, next) => {
+    if (req.user.id !== req.params.id) {
+        return next(errorHandler(401, 'You can only delete your own account!'));
+    }
+    try {
+        await User.findByIdAndDelete(req.params.id);
+        res.clearCookie("token");
+        res.status(200).json("User delete successfully");
+    } catch (error) {
+        next(error);
+    }
+} 
+
+
+
+
 module.exports = {
     test,
-    updateUser
+    updateUser,
+    deleteUser
 }
--- server/Routes/user.routes.js
+++ server/Routes/user.routes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { test, updateUser } = require('../Controller/user.controller');
-const verifyToken  = require("../Utils/verifyUser");
+const { test, updateUser, deleteUser } = require('../Controller/user.controller');
+const verifyToken = require("../Utils/verifyUser");
 
 router.get("/test", test);
-router.post("/update/:id", verifyToken, updateUser)
+router.post("/update/:id", verifyToken, updateUser);
+router.delete("/delete/:id", verifyToken, deleteUser);
 
 module.exports = router;
