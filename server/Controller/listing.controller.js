81d66cecbe184668ab0ab37c1ebc7b03e484f028
MuhammadUmar7195 <mmuhammadumar869@gmail.com>
2025-04-25 15:11:16 +0500

    Create update listing api route

--- server/Controller/listing.controller.js
+++ server/Controller/listing.controller.js
@@ -11,8 +11,8 @@ const createListing = async (req, res) => {
 }
 
 const deleteListing = async (req, res, next) => {
-    const listing2 = await Listing.findById(req.params.id); 
-    
+    const listing2 = await Listing.findById(req.params.id);
+
     if (!listing2) {
         return next(errorHandler(404, "Listing not found."));
     }
@@ -28,7 +28,28 @@ const deleteListing = async (req, res, next) => {
     }
 }
 
+const updateListing = async (req, res, next) => {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) {
+        return next(errorHandler(404, "Listing not found"));
+    }
+    if (req.user.id !== listing.userRef) {
+        return next(errorHandler(401, "You can only update your own listings"));
+    }
+    try {
+        const updateListing = await Listing.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true }
+        );
+        res.status(200).json({ success: true, body: updateListing });
+    } catch (error) {
+        next(error);
+    }
+}
+
 module.exports = {
     createListing,
-    deleteListing
+    deleteListing,
+    updateListing
 }
--- server/Routes/listing.route.js
+++ server/Routes/listing.route.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { createListing, deleteListing } = require("../Controller/listing.controller");
+const { createListing, deleteListing, updateListing } = require("../Controller/listing.controller");
 const verifyToken = require("../Utils/verifyUser");
 
 router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
+router.put("/update/:id", verifyToken, updateListing);
 
 module.exports = router;
